2234faa7175414bfbee6dc1f153d013827f18168
JoseSousaCastro <152998913+JoseSousaCastro@users.noreply.github.com>
2024-04-02 11:04:41 +0100

    2nd commit

--- project5backend/src/main/java/project4/service/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package project4.service;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class ApplicationConfig extends Application {
-}
